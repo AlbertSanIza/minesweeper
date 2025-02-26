22eced575d9a587fc4d9268043de93153cdcd69f
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-25 23:40:47 -0800

    Refactor difficulty settings display and update layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,11 @@ import { useState } from 'react'
 export default function App() {
     const [difficulty, setDifficulty] = useState('easy')
 
-    const getSettings = () => {
-        switch (difficulty) {
-            case 'medium':
-                return { rows: 14, cols: 18, mines: 40 }
-            case 'hard':
-                return { rows: 20, cols: 24, mines: 99 }
-            case 'easy':
-            default:
-                return { rows: 8, cols: 10, mines: 10 }
-        }
-    }
-
     return (
         <div className="fixed flex size-full flex-col bg-amber-50">
             <div className="flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
                 <select
-                    className="rounded-lg border-4 border-black bg-amber-50 px-2 py-1 font-bold text-black"
+                    className="rounded-xl border-4 border-black bg-amber-50 px-2 py-1 font-bold text-black"
                     value={difficulty}
                     onChange={(e) => setDifficulty(e.target.value)}
                 >
@@ -32,7 +20,9 @@ export default function App() {
                     <FlagIcon className="text-red-400" />
                 </div>
             </div>
-            <div className="flex-1">{JSON.stringify(getSettings())}</div>
+            <div className="flex flex-1 items-center justify-center overflow-hidden p-8">
+                <div className="size-full max-h-[100vw] rounded-xl border-4 bg-red-300">hello world</div>
+            </div>
         </div>
     )
 }
