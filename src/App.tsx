e6613e5fa630dce65323048a0847ee900d47bbdf
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 23:27:33 -0800

    Refactor App component to adjust max height for improved layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
             <div className="fixed size-full bg-amber-100">
                 <TopBar />
                 <main className="flex size-full items-center justify-center overflow-hidden p-2 pt-18 sm:p-8 sm:pt-24">
-                    <div className="size-full overflow-hidden rounded-xl border-4 bg-black sm:max-h-[calc(100vw-64px)] sm:max-w-[calc(100vh-128px)]">
+                    <div className="size-full max-h-[calc(100vw-16px)] overflow-hidden rounded-xl border-4 bg-black sm:max-h-[calc(100vw-64px)] sm:max-w-[calc(100vh-128px)]">
                         <Board />
                     </div>
                 </main>
