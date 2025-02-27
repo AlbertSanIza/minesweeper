d792eb400bb97cad4c34e8cb010c33e1bd8cbb00
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 22:38:25 -0800

    Adjust layout padding and max dimensions for improved responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ export default function App() {
         <>
             <div className="fixed size-full bg-amber-100">
                 <TopBar />
-                <main className="flex size-full items-center justify-center overflow-hidden p-8 pt-24">
-                    <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4 bg-black">
+                <main className="flex size-full items-center justify-center overflow-hidden p-2 pt-18 sm:p-8 sm:pt-24">
+                    <div className="size-full overflow-hidden rounded-xl border-4 bg-black sm:max-h-[calc(100vw-64px)] sm:max-w-[calc(100vh-128px)]">
                         <Board />
                     </div>
                 </main>
