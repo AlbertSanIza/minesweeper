d87dd328fd03e9da5c7d58efffef6c7713411fb1
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 22:30:18 -0800

    Add Message component to App for user notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Board from './components/Board'
+import Message from './components/Message'
 import TopBar from './components/TopBar'
 
 export default function App() {
     return (
-        <div className="fixed size-full bg-amber-100">
-            <TopBar />
-            <main className="flex size-full items-center justify-center overflow-hidden p-8 pt-24">
-                <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4 bg-black">
-                    <Board />
-                </div>
-            </main>
-        </div>
+        <>
+            <div className="fixed size-full bg-amber-100">
+                <TopBar />
+                <main className="flex size-full items-center justify-center overflow-hidden p-8 pt-24">
+                    <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4 bg-black">
+                        <Board />
+                    </div>
+                </main>
+            </div>
+            <Message />
+        </>
     )
 }
