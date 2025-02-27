1262791510903b0aef050049a93942a1b2ae5a33
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 22:47:51 -0800

    Add ResetButton component to allow game reset functionality

---
+++ src/components/ResetButton.tsx
@@ -0,0 +1,11 @@
+import { useGameStore } from '../lib/store'
+
+export default function ResetButton() {
+    const { resetGame } = useGameStore()
+
+    return (
+        <button className="h-10 cursor-pointer rounded-xl border-4 border-black bg-red-500 px-2 font-bold text-amber-100 hover:opacity-90" onClick={resetGame}>
+            RESET
+        </button>
+    )
+}
