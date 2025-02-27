2b3b96fd455d22a96624c6613892b0120a7c526d
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 21:39:34 -0800

    Add remaining flags display to TopBar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,19 @@
+import { FlagIcon } from 'lucide-react'
+
 import { useGameStore } from '../lib/store'
 import DifficultySelect from './DifficultySelect'
 
 export default function TopBar() {
-    const { resetGame } = useGameStore()
+    const { resetGame, getRemainingFlags } = useGameStore()
 
     return (
         <div className="fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
             <DifficultySelect />
-            <div>
+            <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2">
+                    <FlagIcon />
+                    {getRemainingFlags()}
+                </div>
                 <button className="h-10 cursor-pointer rounded-xl border-4 border-black bg-red-500 px-2 hover:opacity-90" onClick={resetGame}>
                     RESET
                 </button>
