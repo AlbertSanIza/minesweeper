311f570c56344a898fccddfafd179279b05885eb
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 21:34:57 -0800

    Add RESET button to TopBar component for game reset functionality

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,6 +4,9 @@ export default function TopBar() {
     return (
         <div className="fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
             <DifficultySelect />
+            <div>
+                <button className="h-10 cursor-pointer rounded-xl border-4 border-black bg-red-500 px-2">RESET</button>
+            </div>
         </div>
     )
 }
