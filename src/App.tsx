25f8b52bde4623c967ec81342286adad841989ac
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 21:37:26 -0800

    Add background color to game container in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
         <div className="fixed size-full bg-amber-100">
             <TopBar />
             <main className="flex size-full items-center justify-center overflow-hidden p-8 pt-24">
-                <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4">
+                <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4 bg-black">
                     <Minesweeper />
                 </div>
             </main>
