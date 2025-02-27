13ee4e7b04953ff02302b651b450d8fb23e4edb4
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 21:22:41 -0800

    Refactor App component to integrate TopBar and simplify game layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import { FlagIcon } from 'lucide-react'
-
 import Minesweeper from './Minesweeper'
-import { useGameStore } from './lib/store'
+import TopBar from './components/Topbar'
 
 export default function App() {
-    const { difficulty, setDifficulty } = useGameStore()
-
     return (
-        <div className="fixed flex size-full flex-col bg-amber-50">
-            <div className="flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
-                <select
-                    className="rounded-xl border-4 border-black bg-amber-50 px-2 py-1 font-bold text-black"
-                    value={difficulty}
-                    onChange={(e) => setDifficulty(e.target.value as 'easy' | 'medium' | 'hard')}
-                >
-                    <option value="easy">EASY</option>
-                    <option value="medium">MEDIUM</option>
-                    <option value="hard">HARD</option>
-                </select>
-                <div>
-                    <FlagIcon className="text-red-400" />
-                </div>
-            </div>
-            <div className="flex flex-1 items-center justify-center overflow-hidden p-8">
+        <div className="fixed size-full bg-amber-100">
+            <TopBar />
+            <main className="flex size-full items-center justify-center overflow-hidden p-8 pt-24">
                 <div className="size-full max-h-[calc(100vw-64px)] max-w-[calc(100vh-128px)] overflow-hidden rounded-xl border-4">
                     <Minesweeper />
                 </div>
-            </div>
+            </main>
         </div>
     )
 }
