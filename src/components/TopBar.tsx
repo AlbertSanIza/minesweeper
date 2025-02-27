26de6400dcb37e25381638abfc755667fbb9592a
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 21:22:30 -0800

    Update TopBar component to fix positioning and ensure it remains fixed at the top

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,7 @@ import DifficultySelect from './DifficultySelect'
 
 export default function TopBar() {
     return (
-        <div className="fixed flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
+        <div className="fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b-4 border-black bg-[#4A752C] px-6 text-white">
             <DifficultySelect />
         </div>
     )
