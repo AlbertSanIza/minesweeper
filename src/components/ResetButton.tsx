5ec85bd558c57afa36b85aeca3892dd250155f45
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-02-26 23:24:19 -0800

    Refactor ResetButton component for improved readability and styling

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -4,7 +4,10 @@ export default function ResetButton() {
     const { reset } = useGameStore()
 
     return (
-        <button className="h-10 cursor-pointer rounded-xl border-4 border-black bg-red-500 px-2 font-bold text-amber-100 hover:opacity-90" onClick={reset}>
+        <button
+            className="h-10 cursor-pointer rounded-xl border-4 border-black bg-red-500 px-2 text-xl font-bold text-amber-100 hover:opacity-90"
+            onClick={reset}
+        >
             RESET
         </button>
     )
